7ea0ca3c87091661e294444a123f984c0d6b12fd
psionic0324 <psionic0324@naver.com>
2021-10-27 14:54:39 +0000

    2ed

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-a, b = map(int, input().split())
-
 def gcd(a, b):
-    while b > 0:
-        a, b = b, a % b
+    while b>0:
+        a, b = b, a%b
     return a
 
-def lcm(a, b):
-    return a * b // gcd(a, b)
+def lcd(a, b):
+    return a*b//gcd(a, b)
 
-print(gcd(a, b))
-print(lcm(a, b))
-#유클리드 호제법
+a1, b1 = map(int, input().split())
+a2, b2 = map(int, input().split())
+
+a3 = a1*b2 + a2*b1
+b3 = lcd(b1, b2)
+if gcd(a3, b3) != 1:
+    b3, a3 = b3/gcd(a3, b3), a3/gcd(a3, b3)
+print(a3, b3)
