4870f4b7adaed228cf4a3415c22055bcc0147893
psionic0324 <56951117+psionic0324@users.noreply.github.com>
2021-10-30 06:47:56 +0000

    20211030_commit

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-# 삽입정렬
-array = [8,4,6,2,9,1,3,7,5]
-n = len(array)
-for i in range(1, n):
-    for j in range(i, 0, - 1):
-		if array[j - 1] > array[j]:
-            array[j - 1], array[j] = array[j], array[j - 1]
-print(array)
+# 병합 정렬
+import sys
+N = int(input())
+nlist = []
+for i in range(N):
+  nlist.append(int(sys.stdin.readline()))
+
+def merge_sort(nlist):
+  if len(nlist) < 2:
+    return nlist
+  mid = len(nlist) // 2
+  left_arr = merge_sort(nlist[:mid])
+  right_arr = merge_sort(nlist[mid:])
+  
+  new_merge = []
+  i = j = 0
+  while i < len(left_arr) and j < len(right_arr):
+    if left_arr[i] < right_arr[j]:
+      new_merge.append(left_arr[i])
+      i += 1
+    else:
+      new_merge.append(right_arr[j])
+      j += 1
+  new_merge += left_arr[i:]
+  new_merge += right_arr[j:]
+  return new_merge
+
+result = merge_sort(nlist)
+for i in result:
+  print(i)
