53aec273f43b67c0cbada809fbe02e4ea8af3897
psionic0324 <56951117+psionic0324@users.noreply.github.com>
2021-11-08 14:47:19 +0000

    20211108commit

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-def star(n):
-  if n == 1:
-     print('*', end="")
-  print(star(n/3)) * 3
-  print(star(n/3), star(n/3))
-  print(star(n/3)) * 3
+def func(n, i, j):
+  if i // n % 3 == 1 and j // n % 3 == 1:
+    print(" ", end="")
+  elif n // 3 == 0:
+    print("*", end="")
+  else:
+    func(n//3, i, j)
 
 n = int(input())
-print(star(n))
+for i in range(n):
+  for j in range(n):
+    func(n, i, j)
+  print()
+"""
+함수의 정의
+  func(n, i, j)
+base condition
+  n = 0 return 0
+재귀 식
+  return func(n-1)
+"""
--- readme.md
+++ readme.md
@@ -8,6 +8,10 @@
 
 - [코딩테스트 문제풀이] 
 (https://www.acmicpc.net)
-
+---
 - [알고리즘 강좌 BYbarkingdog]
 (https://blog.encrypted.gg/919?category=773649)
+
+- [문제집]
+(https://github.com/encrypted-def/basic-algo-lecture/blob/master/workbook.md)
+---
